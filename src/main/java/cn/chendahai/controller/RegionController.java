653d9d840632f52e5e359e96d4f6fa9478b688f8
陈海洋 <1559843332@qq.com>
2020-04-29 16:31:29 +0800

    swagger文档补充

--- src/main/java/cn/chendahai/controller/RegionController.java
+++ src/main/java/cn/chendahai/controller/RegionController.java
@@ -5,6 +5,7 @@ import cn.chendahai.service.RegionService;
 import cn.chendahai.vo.JsonResult;
 import cn.chendahai.vo.JsonResultFactory;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,7 @@ public class RegionController {
     @Autowired
     private RegionService regionService;
 
+    @ApiOperation("根据父id获取区域信息")
     @GetMapping("regionList")
     public JsonResult regionList(Integer pid) {
         if (pid == null) {
--- src/main/java/cn/chendahai/entity/Region.java
+++ src/main/java/cn/chendahai/entity/Region.java
@@ -1,5 +1,7 @@
 package cn.chendahai.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -9,18 +11,25 @@ import lombok.Data;
  * @date 2020/4/25 0025
  */
 @Data
+@ApiModel("区域对象信息")
 public class Region {
 
+    @ApiModelProperty("id值")
     private Integer id;
 
+    @ApiModelProperty("父id值")
     private Integer parentId;
 
+    @ApiModelProperty("区域名称")
     private String regionName;
 
+    @ApiModelProperty("行政区划编码")
     private Integer code;
 
+    @ApiModelProperty("级别：1为province, 2为city, 3为district")
     private Integer regionType;
 
+    @ApiModelProperty("是否为最后一级 0表示不是 1表示是")
     private Boolean last;
 
 }
