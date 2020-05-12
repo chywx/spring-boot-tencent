b77ce7581a5a35ed7c9493fb95a2757af599fdcf
陈海洋 <1559843332@qq.com>
2020-05-12 16:58:49 +0800

    shareTwitter

--- src/main/java/cn/chendahai/controller/ShareController.java
+++ src/main/java/cn/chendahai/controller/ShareController.java
@@ -2,6 +2,7 @@ package cn.chendahai.controller;
 
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -35,4 +36,52 @@ public class ShareController {
             + "</html>";
 
     }
+
+    @GetMapping("/shareTwitter")
+    public String shareTwitter() {
+
+        return "<!DOCTYPE html>\n"
+            + "<html lang=\"en\">\n"
+            + "<head>\n"
+            + "</head>\n"
+            + "  <meta property=\"twitter:url\" content=\"https://www.bangbet.com/\"/>\n"
+            + "  <meta property=\"og:url\" content=\"https://www.bangbet.com/\"/>\n"
+            + "  <meta name=\"twitter:site\" content=\"https://www.bangbet.com/\">\n"
+            + "  <meta name=\"twitter:title\" content=\"This is my plan,let's play together\"/>\n"
+            + "  <meta name=\"twitter:description\" content=\"This is my plan,let's play together\"/>\n"
+            + "  <meta name=\"twitter:card\" content=\"summary_large_image\"/>\n"
+            + "  <meta name=\"twitter:image\" content=\"https://www.bangbet.com/images/activity/bet-share.jpeg\"/>"
+            + "<body>\n"
+            + "  \n"
+            + "<script type=\"text/javascript\">\n"
+            + "\twindow.location.href=\"https://www.cnblogs.com/chywx/\";\n"
+            + "</script>"
+            + "</body>\n"
+            + "</html>";
+
+    }
+
+//    @GetMapping("/shareTwitter")
+//    public String shareTwitterMeta(String title, String description, String image) {
+//
+//        return "<!DOCTYPE html>\n"
+//            + "<html lang=\"en\">\n"
+//            + "<head>\n"
+//            + "  <meta property=\"twitter:url\" content=\"https://www.bangbet.com/\"/>\n"
+//            + "  <meta property=\"og:url\" content=\"https://www.bangbet.com/\"/>\n"
+//            + "  <meta name=\"twitter:site\" content=\"https://www.bangbet.com/\">\n"
+//            + "  <meta name=\"twitter:title\" content=\"" + title + "\"/>\n"
+//            + "  <meta name=\"twitter:description\" content=\"" + description + "\"/>\n"
+//            + "  <meta name=\"twitter:card\" content=\"summary_large_image\"/>\n"
+//            + "  <meta name=\"twitter:image\" content=\"" + image + "\"/>"
+//            + "</head>\n"
+//            + "<body>\n"
+//            + "  \n"
+//            + "<script type=\"text/javascript\">\n"
+//            + "\twindow.location.href=\"https://www.cnblogs.com/chywx/\";\n"
+//            + "</script>"
+//            + "</body>\n"
+//            + "</html>";
+//
+//    }
 }
