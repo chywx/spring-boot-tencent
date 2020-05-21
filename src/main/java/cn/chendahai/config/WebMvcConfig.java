8e3edaa0a980335ef02715a19abd4acdcb66b220
陈海洋 <1559843332@qq.com>
2020-05-21 09:56:36 +0800

    跨域处理

---
+++ src/main/java/cn/chendahai/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package cn.chendahai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 功能描述
+ *
+ * @author chy
+ * @date 2020/5/21 0021
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+            .maxAge(3600)
+            .allowCredentials(true);
+    }
+
+}
