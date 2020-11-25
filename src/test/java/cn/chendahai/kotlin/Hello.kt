a01b4bb4979c098bb5509cb4203443d4faf4df8c
dahai <1559843332@qq.com>
2020-11-25 15:34:22 +0800

    kotlin 依赖引入

---
+++ src/test/java/cn/chendahai/kotlin/Hello.kt
@@ -0,0 +1,17 @@
+package cn.chendahai.kotlin
+
+/**
+ *@author lql
+ *@date 2020/11/24 0024 上午 11:00
+ *Description：
+ */
+
+var age: Int = 0
+var num = 20
+val TYPE: Int = 100
+var name: String = ""
+
+fun main() {
+
+    println(123)
+}
