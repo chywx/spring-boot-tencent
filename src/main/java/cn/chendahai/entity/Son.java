6bce229744ada8a547f1ea7d85dff9f4415eb3d2
dylan <dylan@chendahai.cn>
2022-05-30 22:13:31 +0800

    测试请求参数对象套对象

---
+++ src/main/java/cn/chendahai/entity/Son.java
@@ -0,0 +1,14 @@
+package cn.chendahai.entity;
+
+public class Son {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
